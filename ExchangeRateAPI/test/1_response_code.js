9a54e5b3ad6963d52b5da77f6629e3ca1d96d98c
Baca90 <bjks88@gmail.com>
2019-05-12 17:59:53 +0200

    major corrections

--- ExchangeRateAPI/test/1_response_code.js
+++ ExchangeRateAPI/test/1_response_code.js
@@ -1,73 +1,65 @@
-var chakram = require('chakram'),
-    expect = chakram.expect,
-    XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest,
-    assert = require('chai').assert,
-    defaultCurrency = "AUD",
-    allRatesUrl = "https://blockchain.info/ticker",
-    allCurrencies = ["USD", "JPY", "CNY", "SGD","HKD", "CAD", "NZD"
-    ,"AUD", "CLP", "GBP", "DKK","SEK", "ISK", "BRL"
-    , "EUR", "RUB", "PLN", "THB", "KRW", "TWD"],
-    wrongCurrencyError = "Parameter <currency> with unsupported symbol",
-    wrongNumericValueError = "Parameter <value> with invalid numerical value";
+var chakram = require('chakram');
+var expect = chakram.expect;
+var assert = require('chai').assert;
 
-function getResponse(url){
-    var xmlHttpRequest = new XMLHttpRequest();
-    xmlHttpRequest.open("GET",url,false);
-    xmlHttpRequest.send(null);
+const defaultCurrency = "AUD";
+const TICKER_URL = "https://blockchain.info/ticker";
+const TOTBC_URL = "https://blockchain.info/tobtc";
+const  allCurrencies = ["USD", "JPY", "CNY", "SGD","HKD", "CAD", "NZD",
+                        "AUD", "CLP", "GBP", "DKK","SEK", "ISK", "BRL",
+                        "EUR", "RUB", "PLN", "THB", "KRW", "TWD", "CHF", "INR"]
 
-    return xmlHttpRequest.responseText;
-}    
+describe("tobtc API", function() {
+    
+    chakram.addProperty("tobtc", function(){});
+    chakram.addMethod("error", function (respObj, status, message) {
+        expect(respObj).to.have.status(status);
+        expect(respObj.body).to.equal(message);
+    });
 
-function getConvertToBtcValue(currency, value){
-    var convertUrl = "https://blockchain.info/tobtc?currency="+currency+"&value="+value;
-    return getResponse(convertUrl);       
-}
-
-function getAllRates(){
-    var response = getResponse(allRatesUrl);
-
-    return JSON.parse(response);
-}
-
-describe("Smoke", function() {
-    it("Verify HTTP status 200", function () {
-        return chakram.get(allRatesUrl)
-          .then(function (response) {
-            expect(response).to.have.header("content-type", "application/json;charset=UTF-8");
-            expect(response).to.have.responsetime(3000);
-            return expect(response).to.have.status(200);
+    describe("Smoke", function() {
+        it("Verify HTTP status 200", function () {
+            return chakram.get(TICKER_URL)
+              .then(function (response) {
+                expect(response).to.have.header("content-type", "application/json;charset=UTF-8");
+                expect(response).to.have.responsetime(3000);
+                expect(response).to.have.status(200);
+            });
         });
     });
-});
 
-describe("Currencies", function() {
-    it("Verify if all available currencies are present", function () {
-        var response = getResponse(allRatesUrl);
-        for (var i = 0; i < allCurrencies.length; i++) {
-            assert.include(response, allCurrencies[i], "Object contains currency");
-        }
-    });
-});
-
-describe("Converting", function() {
-    it("Verify AUD converting", function () {
-        var amount = 20000;
-        var receivedResult = parseFloat(getConvertToBtcValue(defaultCurrency, amount));
-        var calculatedResult = parseFloat(amount / getAllRates()[defaultCurrency].sell);
-
-        return assert.closeTo(receivedResult, calculatedResult, 2, "Received and calculated values mismatch");
+    describe("Currencies", function() {
+        it("Verify if all available currencies are present", function () {
+            return chakram
+                .get(TICKER_URL)
+                .then((allRates) => {
+                    expect(Object.keys(allRates.body).sort()).to.eql(allCurrencies.sort());
+                })
+        });
     });
 
-    it("Verify error message for unsupported currency parameter", function () {
-        var receivedResult = getConvertToBtcValue("XXX", 1);
-        return assert.equal(receivedResult, wrongCurrencyError, "Received error message mismatch");
+    describe("Converting", function() {
+        it("Verify AUD converting", function () {
+            var amount = 20000;
+            return Promise.all([
+                chakram.get(`${TOTBC_URL}?currency=${defaultCurrency}&value=${amount}`),
+                chakram.get(TICKER_URL)
+            ]).then (([convertedValue, allRates]) => {
+                actResult = parseFloat(convertedValue.body);
+                expResult = parseFloat(amount / allRates.body[defaultCurrency].sell);
+
+                assert.closeTo(actResult, expResult, 2, "Received and calculated values mismatch");
+            })
+        });
+
+        it("Verify error message for unsupported currency parameter", function () {
+            var response = chakram.get("https://blockchain.info/tobtc?currency=XXX&value=1")
+            return expect(response).to.be.tobtc.error(500, "Parameter <currency> with unsupported symbol");
+        });
+
+        it("Verify error message for unsupported amount parameter", function () {
+            var response = chakram.get("https://blockchain.info/tobtc?currency=AUD&value=X")
+            return expect(response).to.be.tobtc.error(500, "Parameter <value> with invalid numerical value");
+        });
     });
-
-    it("Verify error message for unsupported amount parameter", function () {
-        var receivedResult = getConvertToBtcValue(defaultCurrency, "X");
-        return assert.equal(receivedResult, wrongNumericValueError, "Received error message mismatch");
-    }); 
-});
-
-
-
+})
